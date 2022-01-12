c58b6b2a3f3465409f6127078b80bea845f25886
justin <justinl0318@gmail.com>
2022-01-12 17:08:29 +0800

    new

--- tetris.py
+++ tetris.py
@@ -1,1 +1,21 @@
 import pygame
+import random
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 800))
+
+pygame.display.set_caption("Tetris 2P")
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.fill((255, 0, 0))
+    pygame.display.update()
+
+
